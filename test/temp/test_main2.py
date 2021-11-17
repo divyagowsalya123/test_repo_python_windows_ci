0b189b674142650e819e825f742dcf20a82129dc
alexanderdamiani <alexander.damiani15@gmail.com>
2021-11-16 22:18:18 -0500

    mypy

--- test/temp/test_main2.py
+++ test/temp/test_main2.py
@@ -1,14 +1,14 @@
 from temp import main2
 
 
-def test_sub():
+def test_sub() -> None:
     assert main2.sub(3, 4) == -1
     assert main2.sub(4.5, 4) == 0
     assert main2.sub(3.9, 4) == -1
     assert main2.sub(4.2, 3.8) == 0
 
 
-def test_word_count():
+def test_word_count() -> None:
     assert main2.word_count('arm pod race', 'pod') == 1
     assert main2.word_count('arm pod race', 'lap') == 0
     assert main2.word_count('arm arm arm', 'arm') == 3
--- test/test_main.py
+++ test/test_main.py
@@ -1,14 +1,14 @@
 import main
 
 
-def test_add():
+def test_add() -> None:
     assert main.add(3, 4) == 7
     assert main.add(3.5, 4) == 7
     assert main.add(3.9, 4) == 7
     assert main.add(3.9, 4.1) == 8
 
 
-def test_to_sentence():
+def test_to_sentence() -> None:
     assert main.to_sentence('apple') == 'Apple.'
     assert main.to_sentence('Apple trees') == 'Apple trees.'
     assert main.to_sentence('Apple trees.') == 'Apple trees.'
